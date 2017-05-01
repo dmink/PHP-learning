f4eb612641589f6ccd0288d24bab66ce6ee2d7ae
Dmitry Kostrubiak <dminked@gmail.com>
2017-05-01 12:45:07 +0300

    Ex. 02: cycles - for, while

--- borisov/exercises.php
+++ borisov/exercises.php
@@ -1,2 +1,14 @@
 <?php
 
+for ( $num = 1; $num <= 20; $num +=2 ) {
+    echo $num . '<br>';
+};
+
+$string = 'Hello';
+$string_length = strlen($string);
+$i = 0;
+
+while ( $i < $string_length ) {
+    echo strtoupper( $string{$i} ) . '<br>';
+    $i++;
+}
